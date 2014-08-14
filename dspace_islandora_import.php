a83c299d4cc46a9626ee116c6ab18b15a3563adf
Jacob Sanford <jsanford@unb.ca>
2014-08-13 21:08:02 -0300

    Refactor to accurate classnames

--- dspace_islandora_import.php
+++ dspace_islandora_import.php
@@ -6,7 +6,8 @@
  */
 
 require_once 'inc/DspaceItemBundle.php';
-require_once 'inc/IslandoraDspaceBundleImporter.php';
+require_once 'inc/IslandoraImportBundleWriter.php';
 require_once 'lib/smarty/distribution/libs/Smarty.class.php';
 
-
+$path_to_parse = '';
+$parent_pid = '';
